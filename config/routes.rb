40b645ac3f680f31f46a4fbe4e72322717e0b2d1
Alan Andrade <alan.andradec@gmail.com>
2012-02-19 23:09:15 -0600

    Added the index.html with SVG support.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 PortalDeLocutores::Application.routes.draw do
 	constraints :subdomain=>'admin' do
+		root to: 'login'
+		match	'lobby'		=>	'lobby#index', :contoller=>'lobby'
 		match	'login'		=>	'presenter_sessions#new'
 		match 'logout' 	=> 	'presenter_sessions#destroy'
 		resources :programs do
@@ -7,9 +9,7 @@ PortalDeLocutores::Application.routes.draw do
 				match	'approve', :action=>'approve'
 			end
 		end
-		root to: 'presenter_sessions#new'
 		resources :presenters
 		resource :presenter_session
-		match	'lobby'		=>	'lobby#index', :contoller=>'lobby'
 	end
 end
