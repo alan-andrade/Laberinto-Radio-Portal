430282bb25a00135a462d34296c5a426733f9a43
Alan Andrade <alan.andradec@gmail.com>
2012-03-06 10:06:40 -0600

    changed Bundler require

--- config/application.rb
+++ config/application.rb
@@ -3,9 +3,9 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 module PortalDeLocutores
@@ -42,7 +42,7 @@ module PortalDeLocutores
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.2'
+    config.assets.version = '1.0'
     
   end
 end
