16f1daab936787d122953efee44c32f10ed70b6c
Alan Andrade <alan.andradec@gmail.com>
2012-03-05 23:05:53 -0600

    minor changes to update files into the rails:update sukaaa

--- config/application.rb
+++ config/application.rb
@@ -43,5 +43,6 @@ module PortalDeLocutores
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.1'
+    
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
+
 # Initialize the rails application
 PortalDeLocutores::Application.initialize!
